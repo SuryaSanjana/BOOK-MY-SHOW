ea8525e4f519c9c9e223925f62d52297b31f7da4
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2021-07-12 15:31:03 +0530

    planning

---
+++ planning.js
@@ -0,0 +1,21 @@
+//BOOK MY SHOW PLANNING
+
+
+//-> WE need a reusable carousel
+// two types
+   // ->Hero Carousel
+   // -> common carousel
+ // ->cast and crew  small carousel
+
+//3 UI's are to be build
+      // 1->Home page
+      //2->movie page
+      //3-> categoroies page
+      //->authentication page
+      //->Razorpay payment gateway
+
+//plan
+//->Layouts
+//->Routes
+//->pages
+//->components
