8b9906701742fb4b1ed506f19ac51d66ce8eebd4
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2021-07-17 19:40:22 +0530

    add Movie component

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -21,10 +21,10 @@ const MovieInfo = () => {
         </h1>
         <div className="flex flex-col-reverse gap-3 lg:gap-5 lg:flex-col">
           <div className="text-white font-light flex flex-col gap-2 md:px-4">
-            <h4>4k &bull; English &bull; Action</h4>
-            <h4>1h 53m &bull; Action, Sci-Fi, Thriller &bull; 13+</h4>
+             <h4>4k &bull; English &bull; Action</h4>
+             <h4>1h 53m &bull; Action, Sci-Fi, Thriller &bull; 13+</h4>
           </div>
-          <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
+          <div className="flex items-center gap-3 px-2 md:px-4 w-screen md:w-screen lg:w-full">
             <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
               Rent ₹149
             </button>
