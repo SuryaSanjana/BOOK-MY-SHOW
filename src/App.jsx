aa18d603c7cafac0f6ed90197245ff018dfce850
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2021-07-17 13:40:47 +0530

    add movie layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 //  importing HOC
 import DefaultHOC from "./HOC/Default.HOC";
-
+import MovieHOC from "./HOC/Movie.HOC";
 // Components
 // import Temp from "./components/temp";
 //Components-pages
@@ -14,12 +14,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
+
 function App() {
   return (
     <>
      
       <DefaultHOC path="/" exact component= {HomePage} />
-      <DefaultHOC path="/" exact component= {MoviePage} />
+      <MovieHOC path="/movie/:id" exact component= {MoviePage} />
     </>
   );
 };
---
+++ src/HOC/Movie.HOC.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+//layout
+import MovieLayout from "../layouts/Movie.layout";
+const MovieHOC=({component :Component,...rest})=>{ 
+    //const Component=component  (or)  component:Component  we need to do this coz components should have a capital letter
+    return(
+        <>
+          <Route 
+             {...rest} 
+             component = {(props)=>(
+               <MovieLayout>
+                   <Component {...props}/> 
+               </MovieLayout>
+             )}
+            />     
+        </>
+    );
+};
+
+export default MovieHOC;
